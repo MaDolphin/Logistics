99a94c1628b6ac7a8af502d8dc63047bae56f68e
MaDolphin <717119hq7@sina.com>
2016-06-28 14:08:45 +0800

    hhk

---
+++ Logistics/Models/ViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Logistics.Models
+{
+    public class ViewModel
+    {
+        public IEnumerable<Booking> BookingModel { get; set; }
+
+        public IEnumerable<Complaint> ComplaintModel { get; set; }
+
+        public IEnumerable<Delivery> DeliveryModel { get; set; }
+
+        public IEnumerable<Dispatch> DispatchtModel { get; set; }
+
+        public IEnumerable<LogDetail> LogDetailModel { get; set; }
+
+        public IEnumerable<Process> ProcessModel { get; set; }
+
+        public IEnumerable<Storage> StorageModel { get; set; }
+
+        public IEnumerable<User> UserModel { get; set; }
+
+    }
+}
