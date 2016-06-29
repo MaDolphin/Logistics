ce787e4e24edf5aaab74186b1c9b45b572631d40
MaDolphin <717119hq7@sina.com>
2016-06-29 16:36:55 +0800

    hhk

--- Logistics/Controllers/NetWorkModelsController.cs
+++ Logistics/Controllers/NetWorkModelsController.cs
@@ -76,10 +76,66 @@ namespace Logistics.Controllers
         public ActionResult SendManager()
         {
             int account = (int)Session["Account"];
-            var list = from a in db.Storage where a.StorageNetwork == account  orderby a.Status, a.BookGetTime ascending select a;
-            CourierModel model = new CourierModel();
-            model.BookingModel = list.ToList();
-            return View();
+            var list = from a in db.Storage where a.StorageNetwork == account orderby a.StorageStatus, a.StorageTime ascending select a;
+            NetWorkModel model = new NetWorkModel();
+            model.StorageModel = list.ToList();
+            return View(model);
+        }
+
+        public ActionResult SendManagerMethod(int? id)
+        {
+            int RandKey = 0;
+            int account = (int)Session["Acccount"];
+            User user = db.User.Find(account);
+            if(account == 6)
+            {
+                Random ran = new Random();
+                RandKey = ran.Next(2, 3);
+            }
+            if (account == 7)
+            {
+                Random ran = new Random();
+                RandKey = ran.Next(4, 5);
+            }
+            Storage storage = db.Storage.Find(id);
+            LogDetail logDetail = db.LogDetail.Find(storage.PackNo);
+
+            //快递单信息更改
+            logDetail.sender = RandKey;
+            db.Entry(logDetail).State = EntityState.Modified;
+            db.SaveChanges();
+
+            //发货信息状态更改
+            storage.StorageStatus = 1;
+            db.Entry(storage).State = EntityState.Modified;
+            db.SaveChanges();
+
+            //派送信息填写
+            Dispatch dispatch = new Dispatch();
+            dispatch.PackNo = logDetail.PackNo;
+            dispatch.PackName = logDetail.PackName;
+            dispatch.ToName = logDetail.ToName;
+            dispatch.ToTel = logDetail.ToTel;
+            dispatch.ToAddresss = logDetail.ToAddress;
+            dispatch.CourierNo = RandKey;
+            dispatch.DispatchTime = System.DateTime.Now;
+            dispatch.DispatchStatus = 0;
+            db.Dispatch.Add(dispatch);
+            db.SaveChanges();
+            int dispatchNo = dispatch.DispatchNo;
+
+            //流程信息填写
+            Process process = db.Process.Find(logDetail.PackNo);
+            process.DispatchNo = dispatchNo;
+            process.DispatchTime = dispatch.DispatchTime;
+            process.CourierNo = dispatch.CourierNo.ToString();
+            process.Location = user.UserName;
+            process.UpdateTime = System.DateTime.Now;
+            db.Entry(process).State = EntityState.Modified;
+            db.SaveChanges();
+
+            return Content("<script>alert('操作成功！');location.href='../NetWorkModels/SendManager';</script>");
+            //return RedirectToAction("SendManager");
         }
     }
 }
